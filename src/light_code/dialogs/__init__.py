bb0223c86de88e6a6d389406b844c9e0640a2e5f
Rony Biswas <ronybiswasinnovative@gmail.com>
2026-09-18 22:53:22 +0600

    feat: add new GoToLineDialog class for go to line input

--- src/light_code/dialogs/__init__.py
+++ src/light_code/dialogs/__init__.py
@@ -15,3 +15,17 @@ class RenameFileDialog(QInputDialog):
 
     def get_file_name(self):
         return self.getText(self, "Rename File", "New file name:")
+
+class GoToLineDialog(QInputDialog):
+    def __init__(self, parent) -> None:
+        super().__init__(parent)
+
+    def get_line_number(self, current_line: int, max_line: int):
+        return self.getInt(
+            self,
+            "Go to Line",
+            f"Line number (1 - {max_line}):",
+            current_line,
+            1,
+            max_line,
+        )
